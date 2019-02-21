eaa67765045fcce6a12f70636383c586044d7554
DownTheMatrix <suija87@gmail.com>
2019-02-21 19:27:35 +0100

    Close navigation after clicking on navlink

--- js/main.js
+++ js/main.js
@@ -3,6 +3,15 @@ const init = () => {
   /* Navigation Menu */
   const MENU = document.querySelector(".menu");
   const NAVLIST = document.querySelector(".navlist");
+  const NAVLINK = document.querySelectorAll(".navlist__item");
+
+  for (let i = 0; i < NAVLINK.length; i++) {
+    NAVLINK[i].addEventListener("click", () => {
+      if (!NAVLIST.classList.contains("active")) {
+        NAVLIST.classList.toggle("active");
+      }
+    });
+  }
 
   MENU.addEventListener("click", () => {
     NAVLIST.classList.toggle("active");
